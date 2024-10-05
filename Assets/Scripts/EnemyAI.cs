6dc4378af93b0ce5c0fffc7b9810483307559176
Andy Pang <andypang1010@gmail.com>
2024-10-05 19:36:27 -0400

    Merging

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -21,25 +21,18 @@ public class EnemyAI : MonoBehaviour
     public Transform playerTransform;
     private NavMeshAgent agent;
 
-
     [Header("Patrolling")]
     public List<Transform> walkpoints;
     public int currentWalkpointIndex = 0;
 
-    //Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
+
+    [Header("Sensors")]
     public float fovAngle = 150f; // Field of view angle (90 degrees)
-    public float sightDistance = 5f; // Max distance the enemy can see
-
+    public float sightDistance = 10f; // Max distance the enemy can see
     public float attackRange = 2f;
-
-    //For switching states
-    public float detectRange;
 
     private Animator animator;
     private int idleHash, walkHash, runHash, attackHash;
-    private int velXHash, velZHash;
 
     private EnemyAttack enemyAttack;
 
@@ -57,9 +50,6 @@ public class EnemyAI : MonoBehaviour
         runHash = Animator.StringToHash("OnRun");
         attackHash = Animator.StringToHash("OnAttack");
 
-        velXHash = Animator.StringToHash("Vel_X");
-        velZHash = Animator.StringToHash("Vel_Z");
-
         // Add current position as a walkpoint
         GameObject homeWalkpoint = new GameObject("WALKPOINT 0");
         homeWalkpoint.transform.position = transform.position;
@@ -71,49 +61,47 @@ public class EnemyAI : MonoBehaviour
     {
         if (!enemyAttack.isStunned) {
             
-            if (IsPlayerInFieldOfView() && IsPlayerVisible())
-            {}
-            // TODO: If player is seen, then check distance to go into Attack or Chase; else, just patrol or stand around
-            float distanceToPlayer = Vector3.Distance(playerTransform.position, transform.position);
+            if (IsPlayerInFieldOfView() && IsPlayerVisible()) {
+                float distanceToPlayer = (playerTransform.position - transform.position).magnitude;
 
-            // Attack
-            if (distanceToPlayer <= attackRange)
-            {
-                SetCurrentState(ActionState.ATTACK);
+                // Attack
+                if (distanceToPlayer <= attackRange)
+                {
+                    agent.SetDestination(playerTransform.position);
 
-                StopMoving(false);
-                agent.SetDestination(playerTransform.position);
+                    SetCurrentState(ActionState.ATTACK);
+                    StopMoving();
+                }
+
+                // Chase
+                else if (distanceToPlayer <= sightDistance)
+                {
+                    agent.SetDestination(playerTransform.position);
+
+                    SetCurrentState(ActionState.CHASE);
+                    ContinueMoving();
+                }
             }
-
-            // Chase
-            else if (distanceToPlayer <= detectRange)
-            {
-                SetCurrentState(ActionState.CHASE);
-
-                ContinueMoving();
-                agent.SetDestination(playerTransform.position);
-            }
-
-            // Player not seen
+            
             else
             {
                 if (walkpoints.Count > 1) {
-                    SetCurrentState(ActionState.PATROL);
-
-                    ContinueMoving();
                     Patrol();
+                    ContinueMoving();
+
+                    SetCurrentState(ActionState.PATROL);
                 }
 
                 else {
-
-                    StopMoving(false);
                     SetCurrentState(ActionState.IDLE);
+
+                    StopMoving();
                 }
             }
         }
 
         else {
-            StopMoving(true);
+            StopMoving();
         }
 
         SetAnimationBool();
@@ -129,7 +117,80 @@ public class EnemyAI : MonoBehaviour
         return currentState;
     }
 
+    private void Patrol() {
+        if (Vector3.Distance(transform.position, walkpoints[currentWalkpointIndex].position) < 1f) {
+            currentWalkpointIndex = (currentWalkpointIndex + 1) % walkpoints.Count;
+        }
 
+        agent.SetDestination(walkpoints[currentWalkpointIndex].position);
     }
 
+    private void StopMoving() {
+        agent.isStopped = true;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    private void ContinueMoving() {
+        agent.isStopped = false;
+
+        GetComponent<Rigidbody>().constraints = 
+            RigidbodyConstraints.FreezeRotationX 
+            | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    private void SetAnimationBool() {
+        switch (currentState) {
+            case ActionState.IDLE:
+                animator.SetBool(idleHash, true);
+                animator.SetBool(walkHash, false);
+                animator.SetBool(runHash, false);
+                animator.SetBool(attackHash, false);
+
+                break;
+
+            case ActionState.PATROL:
+                animator.SetBool(idleHash, false);
+                animator.SetBool(walkHash, true);
+                animator.SetBool(runHash, false);
+                animator.SetBool(attackHash, false);
+
+                break;
+
+            case ActionState.CHASE:
+                animator.SetBool(idleHash, false);
+                animator.SetBool(walkHash, false);
+                animator.SetBool(runHash, true);
+                animator.SetBool(attackHash, false);
+
+                break;
+
+            case ActionState.ATTACK:
+                animator.SetBool(idleHash, false);
+                animator.SetBool(walkHash, false);
+                animator.SetBool(runHash, false);
+                animator.SetBool(attackHash, true);
+
+                break;
+        }
+    }
+    public bool IsPlayerInFieldOfView()
+    {
+        Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        float angleBetweenEnemyAndPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+        return angleBetweenEnemyAndPlayer <= fovAngle / 2f;
+    }
+
+    public bool IsPlayerVisible()
+    {
+        RaycastHit hit;
+        Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        if (Physics.Raycast(transform.position, directionToPlayer, out hit, sightDistance))
+        {
+            if (hit.transform == playerTransform)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
