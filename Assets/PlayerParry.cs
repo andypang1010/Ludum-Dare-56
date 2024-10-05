5f1d451fb26440e823cdedb0683faba32252e9e4
James <zl293@cornell.edu>
2024-10-05 01:57:30 -0400

    added perry system for player

--- Assets/PlayerParry.cs
+++ Assets/PlayerParry.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerParry : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float parryWindow = 0.5f;  // Time window for a successful parry
+    private bool isParrying = false;
+    private float parryStartTime;
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (InputController.Instance.GetParryDown())
+        {
+            TryToParry();
+        }
+        if (isParrying && Time.time > parryStartTime + parryWindow)
+        {
+            isParrying = false;
+        }
+    }
+
+    void TryToParry()
+    {
+        isParrying = true;
+        parryStartTime = Time.time;
+    }
+    public void SuccessfulParry()
+    {
+        Debug.Log("Parry Successful!");
+        isParrying = false;
+    }
+
+    public bool IsParrying()
+    {
+        return isParrying;
     }
 }
